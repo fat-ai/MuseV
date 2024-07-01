07f456ed5715fa753686dff0961002a76d973538
fat-ai <67062092+fat-ai@users.noreply.github.com>
2024-07-01 12:03:13 +0200

    Update T2I_all_model.py

--- configs/model/T2I_all_model.py
+++ configs/model/T2I_all_model.py
@@ -2,7 +2,7 @@ import os
 
 
 T2IDir = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "../../checkpoints", "t2i"
+    os.path.dirname(os.path.abspath(__file__)), "/content/MuseV/checkpoints", "t2i"
 )
 
 MODEL_CFG = {
