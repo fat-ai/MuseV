72f5b93f484e934b2f6406776339dddd04931ef7
fat-ai <67062092+fat-ai@users.noreply.github.com>
2024-07-01 12:07:46 +0200

    Update motion_model.py

--- configs/model/motion_model.py
+++ configs/model/motion_model.py
@@ -2,7 +2,7 @@ import os
 
 
 MotionDIr = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "../../checkpoints", "motion"
+    os.path.dirname(os.path.abspath(__file__)), "/content/MuseV/checkpoints", "motion"
 )
 
 
